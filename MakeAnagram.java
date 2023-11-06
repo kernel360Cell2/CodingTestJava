09dbfbe1a474f570b016f858545265f661cc5a15
Seokhee Park <kiit0901@gmail.com>
2023-11-07 00:37:59 +0900

    풀어서 올렸습니당...

---
+++ MakeAnagram.java
@@ -0,0 +1,44 @@
+package stonehee.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+// 백준 1919번 문제: 애너그램 만들기
+public class MakeAnagram {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
+
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+
+        int cnt = 0;
+
+        TreeMap<Character, Integer> str1Map = getStrMap(str1);
+        TreeMap<Character, Integer> str2Map = getStrMap(str2);
+
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            int cnt1 = str1Map.getOrDefault(ch, 0);
+            int cnt2 = str2Map.getOrDefault(ch, 0);
+            cnt += Math.abs(cnt1 - cnt2);
+        }
+
+        System.out.println(cnt);
+    }
+
+    private static TreeMap<Character, Integer> getStrMap(String str) {
+        TreeMap<Character, Integer> map = new TreeMap<>();
+        for(char ch : str.toCharArray()) {
+            addOrUpdateKeyValue(map, ch);
+        }
+        return map;
+    }
+
+    private static void addOrUpdateKeyValue(TreeMap<Character, Integer> map, char key) {
+        map.compute(key, (k, v) -> (v == null) ? 1 : v + 1);
+    }
+
+
+}
---
+++ SwitchUpperWithLower.java
@@ -0,0 +1,23 @@
+package stonehee.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+// 백준 2744번 문제: 대소문자 바꾸기
+public class SwitchUpperWithLower {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        System.out.println(switchUpperWithLower(str));
+    }
+
+    public static String switchUpperWithLower(String str) {
+        StringBuilder sb = new StringBuilder();
+        for(char ch : str.toCharArray()) {
+            sb.append(Character.isLowerCase(ch) ? Character.toUpperCase(ch) : Character.toLowerCase(ch));
+        }
+        return sb.toString();
+    }
+}
