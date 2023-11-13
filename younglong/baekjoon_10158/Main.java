fed0f7b2765a812074a78abeb32372d3c95b71f5
Younglong <kyl8555@gmail.com>
2023-11-13 17:24:23 +0900

    [코테] 백준 13223, 10158

---
+++ younglong/baekjoon_10158/Main.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] grid = br.readLine().split(" ");
+        String[] position = br.readLine().split(" ");
+        int time = Integer.parseInt(br.readLine());
+
+        int width = Integer.parseInt(grid[0]);
+        int height = Integer.parseInt(grid[1]);
+
+        int x = Integer.parseInt(position[0]);
+        int y = Integer.parseInt(position[1]);
+
+        int posX = (x + time) % (2 * width);
+        int posY = (y + time) % (2 * height);
+
+        if(posX > width) {
+            posX = 2 * width - posX;
+        }
+
+        if(posY > height) {
+            posY = 2 * height - posY;
+        }
+
+        System.out.println(posX + " " + posY);
+    }
+}
---
+++ younglong/baekjoon_13223/Main.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.time.LocalTime;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] beginTime = br.readLine().split(":");
+        String[] endTime = br.readLine().split(":");
+
+        int secondDiff = getSecondDiff(endTime, beginTime);
+
+        System.out.println(getResult(secondDiff));
+    }
+
+    private static String getResult(int secondDiff) {
+        String result = LocalTime.ofSecondOfDay(secondDiff).toString();
+
+        if (result.length() < 8) {
+            result += ":00";
+        }
+
+        if(result.equals("00:00:00")) {
+            result = "24:00:00";
+        }
+
+        return result;
+    }
+
+    private static int getSecondDiff(String[] endTime, String[] beginTime) {
+        int secondDiff = calculateSecond(endTime) - calculateSecond(beginTime);
+
+        if(secondDiff < 0) {
+            secondDiff += 86400;
+        }
+
+        return secondDiff;
+    }
+
+    private static int calculateSecond(String[] time) {
+        int stnd = 3600;
+        int second = 0;
+
+        for (int i = 0; i < time.length; i++) {
+            second += Integer.parseInt(time[i]) * stnd;
+            stnd /= 60;
+        }
+
+        return second;
+    }
+}
