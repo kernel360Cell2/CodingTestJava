8eb5d2d7e7bb4898061ea894b7379c2a9fa5714d
HyunJunSon <jjang3a@gmail.com>
2023-11-15 07:05:02 +0900

    cell2 study #2

--- HyunJun/week2/backjoon10158.java
+++ HyunJun/week2/backjoon10158.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.stream.IntStream;
 
 public class backjoon10158 {
     public static void main(String[] args) throws IOException {
@@ -23,23 +22,20 @@ public class backjoon10158 {
         int x = location[0];
         int y = location[1];
 
+        int xpos = (x + time) % (width*2);
+        int ypos = (y + time) % (height*2);
+
+        xpos = xpos > width ? width*2 - xpos : xpos;
+        ypos = ypos > height ? height*2 - ypos : ypos;
 
 
-
-
-
-
-
-
-
-
-
+        System.out.println(xpos+" "+ypos);
 
     }
 
     public static int[] toIntArray(String input){
         return
-            Arrays.stream(input.split("\\s*"))
+            Arrays.stream(input.split("\\s"))
                 .mapToInt(Integer::parseInt).toArray();
     }
 }
